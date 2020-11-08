edc82a32df0acf9305633a19db4f47abff7ccdee
Rúben Santos <rubensantos@Rubens-MBP.Home>
2020-11-08 17:49:38 +0000

    REfactor directories

--- projects_compiling/Contrato/src/main/java/INotification.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface INotification extends Remote {
-
-    void sendNotification(String message) throws RemoteException;
-
-}
