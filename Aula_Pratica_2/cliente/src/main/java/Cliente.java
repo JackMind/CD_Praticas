bcc777243957e8d775f379f47b5eff374e85b399
Rúben Santos <rubensantos@Rubens-MBP.Home>
2020-11-16 21:41:41 +0000

    Added control contract

--- Aula_Pratica_2/cliente/src/main/java/Cliente.java
+++ Aula_Pratica_2/cliente/src/main/java/Cliente.java
@@ -1,5 +1,22 @@
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class Cliente {
+
+    static String svcIP="35.230.146.225";
+    static int svcPort=6000;
+
     public static void main(String[] args) {
+
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress(svcIP, svcPort)
+                .usePlaintext().build();
+        // call pingServer
+        Controlo
+        Msg request = Msg.newBuilder().setTxt("luis").build();
+        ServiceAulaGrpc.ServiceAulaBlockingStub blockingStub = 	ServiceAulaGrpc.newBlockingStub(channel);
+        Msg rpy = blockingStub.pingServer(request);
+        System.out.println("result =" + rpy.getTxt());
 
     }
 }
